7678b1a095eb87762028aa8f0c0aba03295de053
odiseo153 <odiseorincon@gmail.com>
2025-09-08 13:48:24 +0000

    arreglando factory de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->unique()->userName(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'position_id' => null,
         ];
     }
 
