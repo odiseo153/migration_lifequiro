a3c9ccd902149541a335b5537f4e8107fadb24b1
odiseo153 <odiseorincon@gmail.com>
2025-09-08 09:42:13 +0000

    subiendo endpoint para migrar

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
-use App\Models\Patient;
 use Illuminate\Http\Request;
-use App\Models\Legacy\Centro;
 use Illuminate\Support\Facades\Route;
+use App\Console\Commands\MigratePlanes;
+use App\Console\Commands\MigrateCentros;
+use App\Console\Commands\MigratePatients;
+use App\Console\Commands\MigratePlanesAsignados;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/migrate-patients', function (Request $request) {
-return  Patient::get();
+Route::get('/migrate-all', function (Request $request) {
+MigrateCentros::handle();
+MigratePatients::handle();
+MigratePlanes::handle();
+MigratePlanesAsignados::handle();
+
+return 'Migración completada';
 });
