495c8e926b6c91f08363fa8c9cb595039aa527e3
odiseo153 <odiseorincon@gmail.com>
2025-09-12 14:52:57 +0000

    asignando la entidad a compra

--- app/Models/Legacy/Compra.php
+++ app/Models/Legacy/Compra.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compra extends Model
 {
-    //
+    protected $connection = 'legacy';
+    protected $table = 'compras';
+    public $timestamps = false; // la tabla vieja no tiene created_at ni updated_at
 }
